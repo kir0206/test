ee63950c632f667721f8e1f917ea7fd00720ede7
Kirill Polikarpov <kir0206@inbox.ru>
2020-12-23 02:02:25 +0700

    Добавление функций

--- test.c
+++ test.c
@@ -1,13 +1,43 @@
 #include <stdio.h>
- 
+#include <assert.h>
+
+int Sum(float a, float b){
+   float y;
+   y=a+b;
+   return (y);
+}
+int inc(float a, float b){
+   float y;
+   y=a-b;
+   return (y);
+}
+int dec(float a, float b){
+   float y;
+   y=a/b;
+   return (y);
+}
+
+int umn(float a, float b){
+   float y;
+   y=a*b;
+   return (y);
+}
+int del(float a, float b){
+   float y;
+   y=a/b;
+   return (y);
+}
+
 main() {
-   float x,y;
-   char sign='+';
+   float x,y,z;
+   char sign;
    while (sign != '0') {
       printf("Sign: ");
       scanf("%c%*c", &sign);
-      if (sign == '0') { break; }
-      if (sign == '+' || sign == '-' 
+      if (sign=='0'){ 
+         break; 
+         }
+      if (sign=='+' || sign == '-' 
       || sign == '*' || sign == '/') {
          printf("x=");
          scanf("%f%*c", &x);
@@ -15,16 +45,23 @@ main() {
          scanf("%f%*c", &y);
          switch (sign) {
             case '+':
-               printf("%.2f\n", x+y);
+               z=Sum(x,y);
+               printf("%.2f\n", &z);
                break;
             case '-':
-               printf("%.2f\n", x-y);
+               z=inc(x,y);
+               printf("%.2f\n", &z);
                break;
             case '*':
-               printf("%.2f\n", x*y);
+               z=umn(x,y);
+               printf("%.2f\n", &z);
                break;
             case '/':
-               if (y != 0) printf("%.2f\n", x/y);
+               if (y != 0)
+                {z=del(x,y); 
+                printf("%.2f\n", &z);
+                }
+
                else printf("Wrong\n");
          }
       }
