1efe15051c873446d640c4c2fb7ad3ad1f129869
Kirill Polikarpov <kir0206@inbox.ru>
2020-12-23 17:45:23 +0700

    Выравнивание кода

--- test.c
+++ test.c
@@ -1,60 +1,70 @@
 #include <stdio.h>
 #include <assert.h>
 
-void Sum(float a, float b){
+void Sum(float a, float b)
+{
    float y;
-   y=a+b;
-  printf("%f\n", y);
+   y = a + b;
+   printf("%f\n", y);
 }
-void inc(float a, float b){
+void inc(float a, float b)
+{
    float y;
-   y=a-b;   
-  printf("%f\n", y);
+   y = a - b;
+   printf("%f\n", y);
 }
-void umn(float a, float b){
+void umn(float a, float b)
+{
    float y;
-   y=a*b;
-  printf("%f\n", y);
+   y = a * b;
+   printf("%f\n", y);
 }
-void del(float a, float b){
+void del(float a, float b)
+{
    float y;
-   y=a/b;
-  printf("%f\n", y);
+   y = a / b;
+   printf("%f\n", y);
 }
 
-int main() {
-   float x,y,z;
+int main()
+{
+   float x, y, z;
    char sign;
-   while (sign != '0') {
+   while (sign != '0')
+   {
       printf("Sign: ");
       scanf("%c%*c", &sign);
-      if (sign=='0'){ 
-         break; 
-         }
-      if (sign=='+' || sign == '-' 
-      || sign == '*' || sign == '/') {
+      if (sign == '0')
+      {
+         break;
+      }
+      if (sign == '+' || sign == '-' || sign == '*' || sign == '/')
+      {
          printf("x=");
          scanf("%f%*c", &x);
          printf("y=");
          scanf("%f%*c", &y);
-         switch (sign) {
-            case '+':
-               Sum(x,y);
-               break;
-            case '-':
-               inc(x,y);
-               break;
-            case '*':
-               umn(x,y);
-               break;
-            case '/':
-               if (y != 0)
-                {del(x,y);
-                }
-
-               else printf("Wrong\n");
+         switch (sign)
+         {
+         case '+':
+            Sum(x, y);
+            break;
+         case '-':
+            inc(x, y);
+            break;
+         case '*':
+            umn(x, y);
+            break;
+         case '/':
+            if (y != 0)
+            {
+               del(x, y);
+            }
+            else
+               printf("Wrong\n");
          }
       }
-      else printf("Wrong sign\n");
+      else
+         printf("Wrong sign\n");
    }
 }
