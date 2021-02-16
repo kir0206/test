6345bc605e8a20561cbc4600ef4b85281df727ab
Kirill_Polikarpov <kir0206@inbox.ru>
2021-02-16 20:55:47 +0700

    Готовая работа

---
+++ Multi.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define ARG_COUNT 4
+
+int main(const int argc, const char *argv[]) {
+   float  first1,  second2;
+   char sign1;
+    if (ARG_COUNT > argc) {
+        printf("Choose sign:\n+)\n-)\n*)\n/)\n");
+        scanf("%c", &sign1);
+        printf("Input x:\n");
+        scanf("%f", &first1);
+        printf("Input y:\n");
+    scanf("%f", &second2);    
+    }
+    //часть кода для тестов
+    else if (argc == ARG_COUNT)
+    {
+    const float sign = atof(argv[1]);
+    const float first = atof(argv[2]);
+    const float second = atof(argv[3]);
+    sign1=sign;
+    first1=first;
+    second2=second;
+    }
+    //часть кода для работы в терминале
+if (sign1 == '+')
+{
+  printf("The result is: %f\n", first1 + second2);
+}
+   else if (sign1 == '-')
+{
+   printf("The result is: %f\n", first1 - second2);
+}
+   else if (sign1 == '*')
+{
+   printf("The result is: %f\n", first1 * second2);
+}
+   else if (sign1 == '/')
+{
+   if (second2 != 0)
+                {printf("The result is: %f\n", first1 / second2);
+                }
+
+               else printf("Error\n");
+         }
+   else
+{
+   printf("Wrong sign");
+}
+//часть кода для тестов
+if (sign1 == 1)
+{
+printf("The result is: %f\n", first1 + second2);
+}
+else if (sign1 == 2)
+{
+   printf("The result is: %f\n", first1 - second2);
+}
+else if (sign1 == 3)
+{
+   printf("The result is: %f\n", first1 * second2);
+}
+else if (sign1 == 4)
+{
+   if (second2 != 0)
+                {printf("The result is: %f\n", first1 / second2);
+                }
+
+               else printf("Error\n");
+         }
+    return EXIT_SUCCESS;
+    getchar();
+}
---
+++ README.rst
@@ -0,0 +1,2 @@
+.. image:: https://www.travis-ci.com/kir0206/test.svg?branch=main
+    :target: https://travis-ci.com/kir0206/test
